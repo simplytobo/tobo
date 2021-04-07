2b7d451bf45b1ce980d2072f60444fe9cfb47a68
simplytobo <81670340+simplytobo@users.noreply.github.com>
2021-04-07 20:07:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 
 
-var x = document.getElementById("feedback");
+var x = document.getElementById("");
 
 getLocation();
 
+
 function loadMap(){
+
+var marker = new ol.Overlay({
+	element: document.getElementById('location'),
+	position: ol.proj.fromLonLat([userLongitude,userLatitude]),
+	positioning: 'bottom-left',
+	stopEvent: false
+	});
+	
+var view = new ol.View({
+    center: ol.proj.fromLonLat([userLongitude, userLatitude]),
+    zoom: 15
+  
+})
 	
 var map = new ol.Map({
   target: 'map',
@@ -13,28 +27,27 @@ var map = new ol.Map({
       source: new ol.source.OSM()
     })
   ],
-  view: new ol.View({
-    center: ol.proj.fromLonLat([userLongitude, userLatitude]),
-    zoom: 4
-  })
+  view: view
 });
 
-var layer = new ol.layer.Vector({
-  source: new ol.source.Vector({
-    features: [
-        new ol.Feature({
-            geometry: new ol.geom.Point(ol.proj.fromLonLat([userLongitude,userLatitude]))
-        })
-    ]
-  })
- });
- map.addLayer(layer);
-console.log(userLatitude);
-console.log(userLongitude);
+// var layer = new ol.layer.Vector({
+  // source: new ol.source.Vector({
+    // features: [
+        // new ol.Feature({
+            // geometry: new ol.geom.Point(ol.proj.fromLonLat([userLongitude,userLatitude]))
+
+        // })
+    // ]
+  // })
+ // });
+ //map.addLayer(layer);
+ console.log(userLongitude);
+  console.log(userLatitude);
+  
+ map.addOverlay(marker);
+
+
 }
-
-
-
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -49,5 +62,9 @@ function showPosition(position) {
   userLongitude = position.coords.longitude;
 
   loadMap();
+  
+   
 }
 
+
+ 
